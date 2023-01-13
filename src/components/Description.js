570fda6d4cfd1f0e0fd307d99211b96172a93900
Souvick Chakraborty <114563269+githubsouvick7@users.noreply.github.com>
2023-01-13 22:44:54 +0530

    Create Description.js

---
+++ src/components/Description.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Description() {
+    return (
+        <p data-ns-test="Project-description">
+            lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto facere amet assumenda aperiam ut eveniet eaque voluptatibus non libero, suscipit iste, voluptates, cupiditate dolorum doloribus enim ratione quisquam vitae? Nihil similique et, sed fuga eveniet deserunt, nulla aspernatur,
+        </p>
+    )
+}
+
+export default Description;
